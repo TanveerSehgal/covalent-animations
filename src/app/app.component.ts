5115ce0acf0e69c966a46860482ade8a155243a5
Tanveer Sehgal <tanveersehgal@gmail.com>
2017-11-18 21:16:16 +0530

    Add collapse animation demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {
   TdRotateAnimation,
   TdBounceAnimation,
+  TdCollapseAnimation,
 } from '@covalent/core';
 
 @Component({
@@ -12,16 +13,19 @@ import {
       TdRotateAnimation(),
       TdRotateAnimation({ anchor: 'customRotate', duration: 750, degrees: 45, ease: 'linear' }),
       TdBounceAnimation({ anchor: 'bounce', duration: 750 }),
+      TdCollapseAnimation({ anchor: 'collapse' })
   ],
 })
 export class AppComponent {
   triggerState = false;
   customTriggerState = false;
   bounceState = false;
+  collapseState = true;
 
   resetAllAnimations() {
     this.triggerState = false;
     this.customTriggerState = false;
     this.bounceState = false;
+    this.collapseState = true;
   }
 }
